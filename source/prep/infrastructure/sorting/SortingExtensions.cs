82dac85a89bff00063b2bd6297c798143c10ed73
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-10-18 16:15:49 -0600

    Updated

---
+++ source/prep/infrastructure/sorting/SortingExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prep.infrastructure.sorting
+{
+    public static class SortingExtensions
+    {
+        public static IComparer<ItemToSort> then_by<ItemToSort, PropertyType>(this IComparer<ItemToSort> comparer,
+                                                                              Func<ItemToSort, PropertyType> accessor)
+        {
+        }
+    }
+}
