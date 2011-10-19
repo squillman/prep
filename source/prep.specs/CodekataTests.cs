51abb071169978be1b705fab62784e9f60fcc6dc
git_name <git_email>
2011-10-19 11:42:28 -0400

    Committing

---
+++ source/prep.specs/CodekataTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using prep.codekata.tuesday;
+
+namespace codekata.tests
+{
+    [TestClass]
+    public class CodeKataTests
+    {
+        [TestMethod]
+        public void default_word_list_should_all_return_true()
+        {
+            BloomFilter sut = new BloomFilter();
+            Assert.IsTrue(sut.IsInDictionary("rhinocerous"));
+            Assert.IsTrue(sut.IsInDictionary("elephant"));
+            Assert.IsTrue(sut.IsInDictionary("donkey"));
+            Assert.IsTrue(sut.IsInDictionary("crocodile"));
+            Assert.IsTrue(sut.IsInDictionary("shawn"));
+            Assert.IsTrue(sut.IsInDictionary("erin"));
+            Assert.IsTrue(sut.IsInDictionary("Erin"));
+            Assert.IsTrue(sut.IsInDictionary("Brennan"));
+        }
+
+        [TestMethod]
+        public void dictionary_should_return_equal_count_as_word_list_after_init()
+        {
+            BloomFilter sut = new BloomFilter();
+            Assert.AreEqual(sut.WordList.Count,8);
+        }
+    }
+}
--- source/prep/codekata/tuesday/BloomFilter.cs
+++ source/prep/codekata/tuesday/BloomFilter.cs
@@ -10,32 +10,44 @@ namespace prep.codekata.tuesday
     public class BloomFilter
     {
         private BitArray bitMap;
-        private IList<string> dictionary;
+
+        public IList<string> WordList { get; set; }
 
         public BloomFilter()
         {
             bitMap = new BitArray(100000);
-            dictionary = new List<string> {"rhinocerous", "elephant", "donkey", "crocodile"};
+            WordList = new List<string>();
+            InitDictionaryBitMap();
         }
 
-        public int GetBitmapIndexFromHash(string wordToHash, int startIndex)
+        private int GetBitmapIndexFromHash(string wordToHash, int startIndex)
         {
             MD5 md5 = MD5.Create();
             byte[] wordBytes = Encoding.ASCII.GetBytes(wordToHash);
             byte[] hash = md5.ComputeHash(wordBytes);
             byte[] indexBytes = new byte[5];
-            Array.Copy(hash, startIndex, indexBytes, 0, 5);
+            Array.Copy(hash, startIndex, indexBytes, 0, 1);
             int bitMapIndex = BitConverter.ToInt32(indexBytes, 0);
-            return bitMapIndex;
+            return Math.Abs(bitMapIndex);
         }
 
         public void InitDictionaryBitMap()
         {
-            foreach (var word in dictionary)
+            if (WordList.Count == 0)
+            {
+                WordList = new List<string> {"rhinocerous", "elephant", "donkey", "crocodile","shawn","erin","Erin","Brennan"};
+            }
+
+            foreach (var word in WordList)
             {
                 int hashIndex = GetBitmapIndexFromHash(word, 3);
                 bitMap[hashIndex] = true;
             }
+        }
+
+        public bool IsInDictionary(string itemToCheck)
+        {
+            return bitMap[GetBitmapIndexFromHash(itemToCheck, 3)];
         }
     }
 }
